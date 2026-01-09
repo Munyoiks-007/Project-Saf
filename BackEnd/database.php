ecaf1508cf6c12add2ab77906d755b05c1be80b1
Munyoiks <167209005+Munyoiks@users.noreply.github.com>
2026-01-09 22:50:08 +0300

    Migration to PHP

---
+++ BackEnd/database.php
@@ -0,0 +1,9 @@
+<?php
+class DatabaseConfig {
+    const HOST = 'localhost';
+    const DBNAME = 'mojo_db';
+    const USERNAME = 'root';
+    const PASSWORD = 'munyoiks7';
+    const CHARSET = 'utf8mb4';
+}
+?>
